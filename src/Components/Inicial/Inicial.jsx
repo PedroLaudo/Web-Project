9ef4fcbb910fff32ff37391c2b274137566e3fe6
AfonsoGraca25936 <aluno25936@ipt.pt>
2025-01-14 16:57:06 +0000

    continuaçao drastica

---
+++ src/Components/Inicial/Inicial.jsx
@@ -0,0 +1,15 @@
+// src/Components/Inicial/Inicial.jsx
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Inicial = () => {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Página Inicial</h1>
+    </div>
+
+  );
+};
+
+export default Inicial;  // Certifique-se de usar export default
